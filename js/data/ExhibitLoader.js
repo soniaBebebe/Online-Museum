343aba947fee9d5946b511cec296ad16d69ec646
soniaBebebe <sonctonc@gmail.com>
2026-03-10 20:36:49 -0700

    bebebe

--- js/data/ExhibitLoader.js
+++ js/data/ExhibitLoader.js
@@ -8,6 +8,7 @@ export class ExhibitLoader{
         if (!res.ok) throw new Error(`Failed to load exhibits: ${res.status}`);
         return await res.json();
     }
+
     createExhibitObject(exhibit){
         const group = new THREE.Group();
         const standGeo = new THREE.CylinderGeometry(0.6, 0.8, 0.9, 32);
@@ -16,6 +17,7 @@ export class ExhibitLoader{
         stand.position.y = 0.45;
         group.add(stand);
 
+        console.log("MODEL LOADED:", exhibit.model);
         // const itemGeo = new THREE.SphereGeometry(0.35,32,32);
         // const itemMat = new THREE.MeshStandardMaterial({roughness:0.35, metalness:0.15});
         // const item = new THREE.Mesh(itemGeo, itemMat);
@@ -37,9 +39,9 @@ export class ExhibitLoader{
                 const box = new THREE.Box3().setFromObject(model);
                 const center = box.getCenter(new THREE.Vector3());
 
+                const size = box.getSize(new THREE.Vector3());
                 model.position.sub(center);
 
-                const size = box.getSize(new THREE.Vector3());
                 const maxDim=Math.max(size.x, size.y, size.z);
 
                 const targetSize=0.7;
@@ -47,13 +49,23 @@ export class ExhibitLoader{
 
                 model.scale.setScalar(scale);
                 model.position.y+=1.2;
+                if(exhibit.offset){
+                    model.position.x+=exhibit.offset[0];
+                    model.position.y+=exhibit.offset[1];
+                    model.position.z+=exhibit.offset[2];
+                }
                 model.traverse((child)=>{
                     if (child.isMesh){
                         child.userData.exhibit=exhibit;
+                        if (group.userData.pickables)
+                            group.user.Data.pickables.push(child);
                     }
                 });
                 group.add(model);
-    
+                console.log("Loaded:", exhibit.model);
+                model.traverse(child=>{
+                    console.log(child.type);
+                });
             })
         }
         else{
